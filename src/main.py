5f4fc34c9dc7d9e719f9d00355abd2ea3bd4e01a
vixadd <dkroell@acm.org>
2016-09-19 23:06:06 -0400

    Initialized configuration files

---
+++ src/main.py
@@ -0,0 +1,16 @@
+#=====================================================#
+#  This is the main script for the Interoperability
+#
+#      Author: vixadd
+#      Date: 09/19/2016
+#
+#=====================================================#
+
+
+import json
+
+# Load JSON data from config file
+with open('./config.json') as data_file:
+    data = json.loads(data_file.read().replace("\n", ""))
+
+print(data["auvsi"]["host"])
