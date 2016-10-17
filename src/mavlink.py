3b9c885dc30dae158e4bf71044c0514b7a7c3162
ViX <dkroell@acm.org>
2016-10-17 17:58:45 -0400

    added UDP stream module with mavutil implementation

--- src/mavlink.py
+++ src/mavlink.py
@@ -2,7 +2,7 @@
 #								#
 #			Mavlink Library				#
 #								#
-#			Author: vixadd				#
+#			Author: davidkroell    			#
 #			Version: 2016-10-03			#
 #								#
 #	     This is the mavlink library that is used		#
@@ -11,6 +11,8 @@
 #===============================================================#
 
 from pymavlink import mavlinkv11 as mavlink
+from pymavlink import mavutil
+
 import json
 import socket
 import thread
@@ -23,21 +25,16 @@ class Mavlink():
 
         self.new_packet = None
         self.current_packet = ""
-        
+
         thread.start_new_thread(startUDPStream, ())
 
     def startUDPStream(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind((UDP_IP, UDP_PORT))
+        mav = mavutil.mavudp(self.ip+":"+self.port, ())
         while True:
-            data, addr = sock.recvfrom(1024)
-            
-            self.curent_packet = data
-            self.new_packet = True
+            statusPacket = mav.recv()
+            status = mav.recv_msg()
+            if(status!=None):
 
-    def getCurrentMavPacket(self):
+    def getMavPacket(self):
         self.new_packet = None
         return self.current_packet
-    
-
-        
