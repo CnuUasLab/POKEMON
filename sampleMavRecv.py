0aca03e4ab11b01a52b0d8ca506eb325ee9ae7e3
ViX <dkroell@acm.org>
2016-11-03 13:57:36 -0400

    Updated template to development section

--- sampleMavRecv.py
+++ sampleMavRecv.py
@@ -1,8 +1,12 @@
 import socket
+import time
 from pymavlink import mavutil
 
 mav = mavutil.mavudp("0.0.0.0:14550", input=True)
 while True:
-	status = mav.recv_msg()
-	if(status!=None):
-		print status
+	try:
+		status = mav.recv_msg()
+		if(status!=None and status.get_type()=="GLOBAL_POSITION_INT"):
+			print "lon: "+str(float(status.lon)/10000000)+" lat: "+str(float(status.lat)/10000000)+" "+str(time.time())
+	except KeyboardInterrupt:
+		break
