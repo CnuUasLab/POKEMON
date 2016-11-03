f44e15cfe286dca12e86d25f2c76dbd66aac93d7
ViX <dkroell@acm.org>
2016-11-03 13:58:41 -0400

    added sequence of setup and initial telemetry handeling`

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
+#=======================================================#
+# Main class for handeling Interoperability operations. #
+#                                                       #
+#             Author: davidkroell                       #
+#             Version: 0.0.1                            #
+#=======================================================#
 
+from mav import Mavlink
+import json
+import sys
+
+# Extract JSON data from configs.
+with open('./config.json') as data_file:
+	constants = json.load(data_file)
+
+# Instantiate a Mavlink module.
+mavl = Mavlink(constants['mavl-incoming']['host'], constants['mavl-incoming']['port'])
+
+while True:
+	currPacket = mavl.getMavPacket()
+	print currPacket
