0ca5b8913d999262bf583edd2ed8373aea5b82d6
ViX <dkroell@acm.org>
2016-11-03 15:56:55 -0400

    Updated condition variable functionality

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ from mav import Mavlink
 import json
 import sys
 
+
 # Extract JSON data from configs.
 with open('./config.json') as data_file:
 	constants = json.load(data_file)
@@ -17,5 +18,10 @@ with open('./config.json') as data_file:
 mavl = Mavlink(constants['mavl-incoming']['host'], constants['mavl-incoming']['port'])
 
 while True:
-	currPacket = mavl.getMavPacket()
-	print currPacket
+        currPacket = mavl.getMavPacket()
+        if(currPacket != None):
+                # post data to competition server
+                # Notify that a packet has been recieved
+                # post data to the socket server for Electron
+                pass
+        # Update Obstacle Data
