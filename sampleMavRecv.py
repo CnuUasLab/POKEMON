3a39de7d4dd7966eaac12c89d5dd69be4d728516
vixadd <dkroell@acm.org>
2017-01-31 01:13:25 -0500

    staging isolation commit

--- sampleMavRecv.py
+++ sampleMavRecv.py
@@ -4,10 +4,7 @@ from pymavlink import mavutil
 
 mav = mavutil.mavudp("0.0.0.0:14550", input=True)
 while True:
-	try:
-		status = mav.recv_msg()
-		#print "< ------------------------------------------------------------------------------------- >"
-		if(status!=None and status.get_type()=="GLOBAL_POSITION_INT"):
-		#	print "lon: "+str(float(status.lon)/10000000)+" lat: "+str(float(status.lat)/10000000)+" "+str(time.time())
-	except KeyboardInterrupt:
-		break
+	status = mav.recv_msg()
+	#print "< ------------------------------------------------------------------------------------- >"
+	if(status!=None and status.get_type()=="GLOBAL_POSITION_INT"):
+		print "lon: "+str(float(status.lon)/10000000)+" lat: "+str(float(status.lat)/10000000)+" "+str(time.time())
