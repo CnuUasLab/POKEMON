be9a84983c6d6164dd0d3d92ea1d16183b0d723b
vixadd <dkroell@acm.org>
2017-05-11 00:05:25 -0400

    Websocketing integrated with udp packets and mission server time.

--- electron.ui/app.py
+++ electron.ui/app.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
 
+import json
+import thread
+import sys
+
+sys.path.insert(0, "../src/")
+from utils import Utils
+from mav import Mavlink
+from mission import Mission
+
+
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
@@ -13,20 +23,59 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 
+util = Utils()
 
+try:
+        # Extract JSON data from configs.
+        with open('../src/config.json') as data_file:
+                constants = json.load(data_file)
+        util.succLog("Successfully extracted config data")
+
+except IOError:
+        util.errLog("WARNING: Config file not found!")
+        util.errLog("Aborting operation! Make sure config.json exists in the /src directory.")
+        util.errLog("I'm angry so I'm going to dump everything into dumpFile.txt now! GoodBye!!!")
+#       util.dump() - > Being developed.
+        sys.exit(0)
+
+# Start Telemetry module to load data into.
+
+mavl = Mavlink(
+                constants['mavl-incoming']['host'],
+                constants['mavl-incoming']['port']
+              )
+
+
+miss = Mission(
+                constants['auvsi']['host'],
+                constants['auvsi']['port'],
+                constants['auvsi']['username'],
+                constants['auvsi']['password']
+                )
+
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+#
+# Background thread to be run when
+# a client connects to the server being hosted.
+#
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 def background_thread():
 	"""Example of how to send server generated events to clients."""
+
 	count = 0
 	telemetry = {}
+	util.log("Hello - Websocketing thread initiated")
+	socketio.sleep(3)
 
 	"""Example of how to send server generated events to clients."""
 	while True:
-		socketio.sleep(10)
-
 		udpPacket = mavl.getMavPacket()
 		lonLatPacket = " "
 		if(udpPacket != None):
 			if(udpPacket.get_type() == "GLOBAL_POSITION_INT"):
+				socketio.sleep(1)
+				#util.log("GLOBAL_POSITION_INT: Packet Recieved")
+
 				telemPacket = udpPacket
 
 				# populate the longitude element
@@ -35,19 +84,14 @@ def background_thread():
 				telemetry['heading'] = float(telemPacket.hdg)/1000
 				telemetry['altitude'] = float(telemPacket.alt)/10000
 
-                                system_time = miss.getMissionTime()
+                               	system_time = miss.getSystemTime()
 
-				socketio.emit('tele',
-				              {'data': telemetry, 'count': count},
-				                namespace='/test')
-				socketio.emit('srvTime',
-				              {'data': server_time, 'count': count},
-				                namespace='/test')
-                                print system_time
+				socketio.emit('tele', {'data': telemetry, 'count': count}, namespace='/test')
+				socketio.emit('srvTime', {'data': system_time, 'count': count}, namespace='/test')
 
-				socketio.sleep(10)
+				#socketio.sleep(10)
 				count += 1
-				
+
 
 
 @app.route('/')
@@ -77,6 +121,8 @@ def join(message):
     emit('my_response',
          {'data': 'In rooms: ' + ', '.join(rooms()),
           'count': session['receive_count']})
+
+
 
 
 @socketio.on('connect', namespace='/test')
