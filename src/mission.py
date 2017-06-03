9663234eb380fceb7bce66890c99cb4ad0b5a31f
Cameron Calpin <cameron.calpin.14@cnu.edu>
2017-06-03 02:49:31 -0400

    Added Singleton with function to get line color

--- src/mission.py
+++ src/mission.py
@@ -12,7 +12,7 @@ import thread
 from utils import Utils
 
 sys.path.insert(0, "../interop/client/")
-import interop
+#import interop
 
 #==================================
 #
@@ -55,10 +55,10 @@ class Mission():
 
 		self.logged_in = False
 		try:
-			self.client = interop.Client( url=self.host+":"+self.port,
-							username=self.username,
-							password=self.password
-							)
+#			self.client = interop.Client( url=self.host+":"+self.port,
+#							username=self.username,
+#							password=self.password
+#							)
 			self.logged_in = True
 			self.util.succLog("Successfully logged into competition server.")
 
@@ -66,7 +66,7 @@ class Mission():
 			thread.start_new_thread(self.populateMissionComponents, ())
 			self.util.succLog("Successfully started mission retrieval thread.")
 
-		except interop.exceptions.InteropError:
+#		except interop.exceptions.InteropError:
 			self.util.errLog("ERROR: Invalid login to competition server.")
 		except requests.exceptions.ConnectionError:
 			self.util.errLog("Connection error with competition server - Are you sure the Server is Running?")
--- src/utils.py
+++ src/utils.py
@@ -1,43 +1,82 @@
 import os
 
 class bcolors:
-	HEADER = '\033[95m'
-    	OKBLUE = '\033[94m'
-    	OKGREEN = '\033[92m'
-    	WARNING = '\033[93m'
-    	FAIL = '\033[91m'
-    	ENDC = '\033[0m'
-    	BOLD = '\033[1m'
-	SUCCESS = '\033[1;42m'
-    	UNDERLINE = '\033[4m'
-    	ERROR = '\033[1;41m'
+		HEADER = '\033[95m'
+		OKBLUE = '\033[94m'
+		OKGREEN = '\033[92m'
+		WARNING = '\033[93m'
+		FAIL = '\033[91m'
+		ENDC = '\033[0m'
+		BOLD = '\033[1m'
+		SUCCESS = '\033[1;42m'
+		UNDERLINE = '\033[4m'
+		ERROR = '\033[1;41m'
 
-class Utils():
-	def __init__(self):
+# Using this as a Template
+"""
+class Singleton:
+	def __init__(self, decorated):
 		self.logs = []
-		self.currLog = 'Initialized'
+		self._decorated = decorated
+
+	def Instance(self):
+		try:
+			return self._instance
+		except AttributeError:
+			self._instance = self._decorated
+			return self._instance
+
+	def __call__(self):
+		raise TypeError('Singletons must be accessed through `Instance()`.')
+
+	def __instancecheck__(self, inst):
+		return isinstance(inst, self._decorated)
+"""
+
+
+def b_colors(myClass):
+	instances = {}
+	def getColor(first, string, second):
+		if myClass not in instances:
+			instances[myClass] = myClass(first, string, second)
+			print instances[myClass]
+		return instances[myClass]
+	return getColor
+
+
+@b_colors
+class Utils(bcolors):
+	def __init__(self, string):
+		print 'Instance created'
 
 	def log(self, string):
-		print bcolors.BOLD + string + bcolors.ENDC
+#		print bcolors.BOLD + string + bcolors.ENDC
+		x = getColor(BOLD, string, ENDC)
+		print x
 		self.currLog = string
 		self.logs.append(string)
 
 	def errLog(self, string):
-		print bcolors.ERROR + string + bcolors.ENDC
+#		print bcolors.ERROR + string + bcolors.ENDC
+		getColor(ERROR, string, ENDC)
 
 		self.currLog = string
-                self.logs.append(string)
+		self.logs.append(string)
 
 	def succLog(self, string):
-		print bcolors.SUCCESS + string + bcolors.ENDC
+#		print bcolors.SUCCESS + string + bcolors.ENDC
+		getColor(SUCCESS, string, ENDC)
 
 		self.currLog = string
-                self.logs.append(string)
+		self.logs.append(string)
 
 	def getPreviousLogs(self):
 		return self.logs
 
-#	def dump(self):
-#		f = open("./dumpFile.txt","w+")
-#		for (word in self.getPreviousLogs()):
-#			f.write(word)
+	def dump(self):
+		f = open("./dumpFile.txt","w+")
+		for word in self.getPreviousLogs():
+			f.write(word)
+
+x = Utils()
+x.log('Something')
