cefa8e5cf6554818dbff599526281b9dd636f4dc
Cameron Calpin <cameron.calpin.14@cnu.edu>
2017-06-08 12:45:15 -0400

    Testing some variations of Singleton classes

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,5 @@
 import os
 
-class bcolors:
-		HEADER = '\033[95m'
-		OKBLUE = '\033[94m'
-		OKGREEN = '\033[92m'
-		WARNING = '\033[93m'
-		FAIL = '\033[91m'
-		ENDC = '\033[0m'
-		BOLD = '\033[1m'
-		SUCCESS = '\033[1;42m'
-		UNDERLINE = '\033[4m'
-		ERROR = '\033[1;41m'
-
 # Using this as a Template
 """
 class Singleton:
@@ -33,7 +21,7 @@ class Singleton:
 		return isinstance(inst, self._decorated)
 """
 
-
+"""
 def b_colors(myClass):
 	instances = {}
 	def getColor(first, string, second):
@@ -42,12 +30,32 @@ def b_colors(myClass):
 			print instances[myClass]
 		return instances[myClass]
 	return getColor
+"""
+class Singleton(type):
+	def __init__(self, first, string, second):
+		super().__init__()
+		self._instance = None
+
+	def __call__(self, first, string, second):
+		if self._instance is None:
+			self._instance = super().__call__(*args, **kwargs)
+		return self._instance
 
 
-@b_colors
-class Utils(bcolors):
-	def __init__(self, string):
-		print 'Instance created'
+@Singleton
+class Utils:
+
+	class bcolors:
+		HEADER = '\033[95m'
+		OKBLUE = '\033[94m'
+		OKGREEN = '\033[92m'
+		WARNING = '\033[93m'
+		FAIL = '\033[91m'
+		ENDC = '\033[0m'
+		BOLD = '\033[1m'
+		SUCCESS = '\033[1;42m'
+		UNDERLINE = '\033[4m'
+		ERROR = '\033[1;41m'
 
 	def log(self, string):
 #		print bcolors.BOLD + string + bcolors.ENDC
